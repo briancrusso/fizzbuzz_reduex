d9957edb1b2e052df7e42bc798137a2ad3ef6fdd
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-05-14 10:29:00 -0400

    Tried adding NaN modifier

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 var userNum = prompt("Enter a number");
-//   if (+userNum === NaN) {
-//        console.log("I said a number, please.");
+// Where does this statement go?
+//  if (+userNum === NaN) {
+//       var userNum = prompt("I said a number, please.");
+//}
+//  okay, not here
 
-for (var i = 0; i <= +userNum; i++) {
-    if (userNum = NaN) {
-        console.log("I said a number, please.");
-    } else if ((i % 3) === 0 && (i % 5) === 0) {
+for (var i = 0; i < +userNum; i++) {
+    if ((i % 3) === 0 && (i % 5) === 0) {
         console.log("FizzBuzz");
     } else if ((i % 5) === 0) {
         console.log("Buzz");
     } else if ((i % 3) === 0) {
         console.log("Fizz");
+//    } else if (+userNum === NaN) {
+//         var userNum = prompt("I said a number, please."); not here either
     } else {
         console.log(i);
     }
 };
+
+// if (+userNum === NaN) {
+//       var userNum = prompt("I said a number, please.");
+//} not here either... Where?
